544df85dadee8d49321f2e49d962cf2d9dcb57af
Daniel Wright <dan@Daniels-MacBook-Air.local>
2017-05-13 13:29:06 -0400

    mac working

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,56 @@
 var gulp = require('gulp');
-
 var nodemon = require('gulp-nodemon');
 //var livereload = require('gulp-livereload');
-var webpack = require('webpack');
 var browserSync = require('browser-sync').create();
-
-
+var watch = require('gulp-watch');
+//var webpack = require('webpack');
+var webpack = require('webpack-stream');
 
 var path=require("path");
 var DIST_DIR = path.resolve(__dirname, "public");
 var SRC_DIR = path.resolve(__dirname, "dev");
 
-
-
-// gulp.task('default', function() {
-//   return gulp.src('src/entry.js')
-//     .pipe(webpack())
-//     .pipe(gulp.dest('dist/'));
+// gulp.task('webpack', function(done) {
+// // webpack( require('./webpack.config.js') );
+//   console.log('hey');
+//   webpack({
+//   	entry: path.resolve(SRC_DIR, "index.js"),
+//   	output: {
+//   		path: DIST_DIR,
+//   		filename: "bundle.js"
+//   	},
+//   	module: {
+//   		loaders:[
+//   			{
+//   				test:/\.jsx?/,
+//   	 			exclude:/(node_modules)/,
+//   				include: SRC_DIR,
+//   				loader: "babel-loader",
+//   				query: {
+//   					presets: ["react", "es2015"]
+//   				}
+//   			},
+//   			{
+//          			test: /\.css/,
+//           		loaders: ['style-loader', 'css-loader'],
+//           		include: SRC_DIR
+//         		}
+//   		],
+//   	}
+//   });
+//   done();
 // });
-gulp.task('webpack', function(done) {
-// webpack( require('./webpack.config.js') );
-  
-webpack({
-	entry: path.resolve(SRC_DIR, "index.js"),
-	output: {
-		path: DIST_DIR,
-		filename: "bundle.js"
-	},
-	module: {
-		loaders:[
-			{
-				test:/\.jsx?/,
-	 			exclude:/(node_modules)/,
-				include: SRC_DIR,
-				loader: "babel-loader",
-				query: {
-					presets: ["react", "es2015"]
-				}
-			},
-			{
-       			test: /\.css/,
-        		loaders: ['style-loader', 'css-loader'],
-        		include: SRC_DIR
-      		}
 
-
-		],
-
-	}
-}, function(error) {
-        var pluginError;
- 
-        if (error) {
-            pluginError = new gulpUtil.PluginError('webpack', error);
- 
-            if (done) {
-                done(pluginError);
-            } else {
-                gulpUtil.log('[webpack]', pluginError);
-            }
- 
-            return;
-        }
- 
-        if (done) {
-            done();
-        }
-    }
-);
-  console.log('sup');
- // console.log(gulp.src())
-  
-
-});
-
-gulp.task('webpack2', function() {
- webpack( require('./webpack.config.js') );
-return gulp.src('dev/index.js')
-  .pipe(webpack(require('./webpack.config.js'))).pipe(gulp.dest('public/'));;
-  
-
+gulp.task('webpack', function() {
+  console.log('regenerating ');
+  return gulp.src('dev/index.js')
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('public/'));
 });
 
 gulp.task('watch', function(){
 	gulp.watch(['dev/*','webpack.config.js'], ['webpack']); 	
+  //gulp.watch(path.resolve(__dirname,'webpack.config.js'), ['webpack']);  
 });
 
 gulp.task('start', function () {
@@ -105,11 +72,13 @@ gulp.task('browser-sync', function() {
         // server: {
         //     baseDir: "./"
         // },
-        files: ['server.js','public/bundle.js*'],
+        files: ['server.js','public/*', ],
         proxy:'localhost:3000',
         port: 8000
     });
 });
+
+
 
 
 
