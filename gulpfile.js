4439895fd82e5ea4a82293d116e07fde325de91a
Daniel Wright <13wrightd@gmail.com>
2017-05-15 17:00:55 -0400

    gulp works better

--- gulpfile.js
+++ gulpfile.js
@@ -13,13 +13,16 @@ var SRC_DIR = path.resolve(__dirname, "dev");
 gulp.task('webpack', function() {
   console.log('regenerating ');
   return gulp.src('dev/index.js')
-    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(webpack(require('./webpack.config.js')).on('error', function(error) {
+            console.log("webpack error");
+            this.emit('end');
+        })  )
     .pipe(gulp.dest('public/'));
 });
 
 gulp.task('watch', function(){
-	gulp.watch(['dev/*','webpack.config.js'], ['webpack']); 	
-  //gulp.watch(path.resolve(__dirname,'webpack.config.js'), ['webpack']);  
+  gulp.watch(['dev/**/*','webpack.config.js'], ['webpack']);  
+  //gulp.watch(path.resolve(__dirname,'webpack.config.js'), ['webpack']);
 });
 
 gulp.task('start', function () {
@@ -27,14 +30,14 @@ gulp.task('start', function () {
      script: 'server.js'
     // , ignore: []
     ,watch:   ['server.js', 'config.js']
-   
+
  // , ext: 'js html'
 
   , env: { 'NODE_ENV': 'development' }
   }).on('restart', function () {
       setTimeout(function(){
-      	//console.log('reload done')
-      	browserSync.reload()},1000);
+        //console.log('reload done')
+        browserSync.reload()},1000);
     });
 
 });
